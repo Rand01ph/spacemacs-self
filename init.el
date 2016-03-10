8e971caeed7fb07deb869d381c4f93bdd9bca7bc
Rand01ph <361212419@qq.com>
2016-03-10 14:49:36 +0800

    add eyebrowse

--- init.el
+++ init.el
@@ -54,6 +54,7 @@ values."
     (chinese :variables
              chinese-enable-fcitx t)
     osx
+    eyebrowse
    )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
